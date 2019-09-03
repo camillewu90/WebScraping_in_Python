b655b4acc80b2952c59265bc83ff7fb914f1d2bc
camillewu90 <51872168+camillewu90@users.noreply.github.com>
2019-09-02 22:04:35 -0400

    Create ws_reliable_connect.py
    This is a part of the web scraper code that try to catch all the potential error that we minght encounter in web scraping

---
+++ ws_reliable_connect.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup 
+# get the title of the webpage
+def getTitle(url): 
+    #open the webpage
+    try:
+        html = urlopen(url) 
+    #the webpage is not found on the server
+    except HTTPError as e: 
+        return None
+    try:
+        #pass the content of the page to a BeautifulSoup object
+        bsObj = BeautifulSoup(html.read()) 
+        #get the title content of the h1 tag
+        title = bsObj.body.h1
+    #guard if the body tag does not exist
+    except AttributeError as e: 
+        return None
+    return title
+title = getTitle("http://www.pythonscraping.com/exercises/exercise1.html")
+if title == None:
+    print ("title could not be found")
+else:
+    print(title)
+        
+     
